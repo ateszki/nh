45bfd8e2691510226950579d08a4f71ccd104d42
ateszki <ateszki@gmail.com>
2016-03-31 11:25:53 -0300

    manual

---
+++ public/manual/conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+jonathan:$1$4D9gMU0R$UzhPf8DhtwB4ZsgJD1GJ61:Jonathan:jonathan@nubehilados.com:admin,user
