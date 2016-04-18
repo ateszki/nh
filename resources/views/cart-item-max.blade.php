11ae9ad94c6ea215afa7e73e43a5e71d54e4b1e6
ateszki <ateszki@gmail.com>
2016-04-18 11:25:31 -0300

    precios

--- resources/views/cart-item-max.blade.php
+++ resources/views/cart-item-max.blade.php
@@ -8,13 +8,13 @@
 	<td data-cell-title="Quantity">
 		<div class="quantity clearfix t_align_c">
 			<button class="f_left d_block minus black_hover tr_all">&#45;</button>
-			<input type="text" class="f_left color_light item-qty item-qty-carrito" data-rowid='{{$row->id}}' readonly name="" value="{{$row->qty}}">
+			<input type="text" class="f_left color_light item-qty item-qty-carrito" data-rowid='{{$row->id}}' data-urlheader="{{URL::to('carrito/header-cart')}}" data-url="{{URL::to('carrito/update')}}/{{$row->rowid}}" readonly name="" value="{{$row->qty}}">
 			<button class="f_left d_block black_hover tr_all">&#43;</button>
 		</div>
 	</td>
 	<td data-cell-title="Total" class="second_font"><b class="color_dark item-subtotal">${{$row->subtotal}}</b></td>
 	<td class="t_align_c">
-        <button class="button_type_8 black_hover grey state_2 m_xs_bottom_0 tr_all color_dark t_align_c vc_child m_bottom_5 tooltip_container relative shoping-cart-update" data-urlheader="{{URL::to('carrito/header-cart')}}" data-url="{{URL::to('carrito/update')}}/{{$row->rowid}}"><i class="fa fa-check fs_large d_inline_m"></i><span class="tooltip right hidden animated" data-show="fadeInRight" data-hide="fadeOutRight">Actualizar Producto</span></button><br class="d_inline_b">
+        <!--<button class="button_type_8 black_hover grey state_2 m_xs_bottom_0 tr_all color_dark t_align_c vc_child m_bottom_5 tooltip_container relative shoping-cart-update" data-urlheader="{{URL::to('carrito/header-cart')}}" data-url="{{URL::to('carrito/update')}}/{{$row->rowid}}"><i class="fa fa-check fs_large d_inline_m"></i><span class="tooltip right hidden animated" data-show="fadeInRight" data-hide="fadeOutRight">Actualizar Producto</span></button><br class="d_inline_b">-->
   		<button class="button_type_8 black_hover grey state_2 m_xs_bottom_0 tr_all color_dark t_align_c vc_child tooltip_container relative shoping-cart-remove" data-itemid="{{$row->rowid}}" data-url="{{URL::to('carrito/remove')}}/{{$row->rowid}}"><i class="fa fa-times fs_large d_inline_m"></i><span class="tooltip right hidden animated" data-show="fadeInRight" data-hide="fadeOutRight">Eliminar Producto</span></button>
 	</td>
 </tr>
