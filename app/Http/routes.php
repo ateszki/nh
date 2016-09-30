01f68c20856d0b15cbcac6e25e6c6c4255a3b3fe
ateszki <ateszki@gmail.com>
2016-09-30 14:45:28 -0300

    jonathan remitentte

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -101,7 +101,8 @@ Route::match(['get', 'post'],'/contacto', function () {
                         ->withInput();
         }
         $envio = Mail::send('email-contacto', ['request' => Request::all()], function ($m) {
-            $m->from(Request::get('email'), Request::get('nombre'));
+           // $m->from(Request::get('email'), Request::get('nombre'));
+             $m->from('jonathan@nubehilados.com', 'Nube');
 
             $m->to('valeria@nubehilados.com', 'Valeria')->cc('jonathan@nubehilados.com','Jonathan')->subject('Contacto web nube');
         });
