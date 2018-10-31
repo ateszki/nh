ddcd3652cd712a40bd05514250242dbf8a5fa235
ateszki <ateszki@gmail.com>
2018-10-30 22:58:30 -0300

    sparkpost options

--- config/services.php
+++ config/services.php
@@ -36,5 +36,10 @@ return [
     ],
     'sparkpost' => [
         'secret' => env('SPARKPOST_KEY'),
+        'options' => [
+            'open_tracking'  => false,
+            'click_tracking' => false,
+            'transactional'  => true,
+        ],
     ],
 ];
