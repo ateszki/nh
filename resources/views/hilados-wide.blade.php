b96dd7ac0e02d34da45f821fa3cb37e2ab18230a
ateszki <ateszki@gmail.com>
2018-11-12 11:40:54 +0100

    hilados wide

--- resources/views/hilados-wide.blade.php
+++ resources/views/hilados-wide.blade.php
@@ -44,11 +44,11 @@
 								@foreach($hilados as $hilado)
                                 <!-- producto single -->
 								<div class="category_isotope_item">
-									<figure class="product_item type_2 c_image_container relative frame_container bg_white t_sm_align_c r_image_container qv_container">
+									<figure class="product_item type_2 c_image_container relative frame_container bg_white t_sm_align_c r_image_container qv_container" >
 										<!-- producto content -->
 										<div class="relative">
 											<div class="d_block">
-												<a href="{{URL::to('hilados/'.$hilado->codigo)}}"><img src="{{URL::to('prodimag/'.$hilado->imagen.'-D.jpg')}}" alt="" class="c_image_1 tr_all" border="0"></a>
+												<a href="{{URL::to('hilados/'.$hilado->codigo)}}"><img src="{{URL::to('prodimag/'.$hilado->imagen.'-Z.jpg')}}" alt="" class="c_image_1 tr_all" border="0"></a>
 											</div>
 											<!--oferta <div class="product_label fs_ex_small circle color_white bg_lbrown t_align_c vc_child tt_uppercase"><i class="d_inline_m">Oferta!</i></div>	-->
 										</div>
