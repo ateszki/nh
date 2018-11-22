bd2f609a566c4b744558f821e5bfe3ee39158dc2
ateszki <ateszki@gmail.com>
2018-11-22 11:35:25 -0300

    ajustes mail jonathan

--- resources/views/colores-wide.blade.php
+++ resources/views/colores-wide.blade.php
@@ -35,7 +35,7 @@
                               </div>
                               <div class="container">
                                   <div class="row">
-                                      <div class="col-lg-4 col-lg-offset-4 col-md-4 col-md-offset-4 col-sm-4 col-sm-offset-4">
+                                      <div class="col-lg-8 col-lg-offset-2 col-md-4 col-md-offset-4 col-sm-4 col-sm-offset-4">
                                       	<img src="{{URL::to('prodimag/'.$hilado->imagen.'-Z.jpg')}}" />
                                       </div>
                                   </div>
